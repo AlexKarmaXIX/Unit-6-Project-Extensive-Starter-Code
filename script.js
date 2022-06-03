d8f0bafdc326ed3478fbfd822aebfe14a81cf9b8
AlexKarmaXIX <106821356+AlexKarmaXIX@users.noreply.github.com>
2022-06-03 10:17:51 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".yes-button").click(function(){
+    $(".Part-Two-Yes").show();
+    $(".treasure-img").show();
+    $(".Story-Ending-Yes").show();
+});
+
+$(".no-button").click(function(){
+     $(".Part-Two-No").show();
+    $(".treasure-img").show();
+    $(".Story-Ending-No").show();
+
+});
